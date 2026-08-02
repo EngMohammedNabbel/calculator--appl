750e2752fda129c39ee0647d7f9af66aeedf5d2f
Alhommed Allows <alhommedallows@gmail.com>
2026-08-02 16:31:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator App
+This is simple C++ clculator project .
+## Feature 
+
+-Add tow numbers .
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int add(int a , int b)
+{
+  return a + b;
+}
+
+int main()
+
+{
+    cout << "Calculator App "<< endl;
+    cout << " 2 + 3 = "<<add(2,3)<< endl;
+    return 0;
+}
