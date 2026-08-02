afea8bc2cc98fcc9ab3a3d7f15f024130c4c6a50
Alhommed Allows <alhommedallows@gmail.com>
2026-08-02 17:07:42 +0300

    Add multiple function

--- main.cpp
+++ main.cpp
@@ -4,11 +4,22 @@ int add(int a , int b)
 {
   return a + b;
 }
+int subtract(int a , int b)
+{
+  return a - b;
+}
+int multiple(int a , int b)
+{
+  return a * b;
+}
 
 int main()
 
 {
     cout << "Calculator App "<< endl;
     cout << " 2 + 3 = "<<add(2,3)<< endl;
+    cout << " 5 - 2 = "<<subtract(5,2)<< endl;
+    cout << " 5 * 2 = "<<multiple(5,2)<< endl;
+
     return 0;
 }
